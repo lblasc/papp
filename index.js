ba2115431bed44dd73ea43be4ad965be24c7d504
Luka Blaskovic <lblasc@znode.net>
2019-10-10 13:52:50 +0200

    use puppeteer-core

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-core')
 const express = require('express')
  
 async function printPDF() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, executablePath: process.env.CHROME });
   const page = await browser.newPage();
   await page.goto('https://lobste.rs/recent', { waitUntil: 'networkidle2' });
   const pdf = await page.pdf({ format: 'A4' });
